a16a8b504ed2f743674394b07ca7533bc8a959fc
Reddie <taban.chol@student.moringaschool.com>
2025-01-06 00:57:37 +0300

    completes code challenge

--- server/models.py
+++ server/models.py
@@ -16,13 +16,14 @@ class Customer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-
+    reviews = db.relationship('Review', back_populates='customer')
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
 class Item(db.Model):
     __tablename__ = 'items'
+    reviews = db.relationship('Review', back_populates='item')
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -30,3 +31,17 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id= db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+   
+    item = db.relationship('Item', back_populates ='reviews')
+    customer = db.relationship('Customer', back_populates ='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.content}, {self.rating}>'
